0de4901e4c4da8cbf4393798a599cb391753b62d
Karames <karames@gmail.com>
2019-10-15 22:56:41 +0200

    update

--- 01_basico/01_holamundo.dart
+++ 01_basico/01_holamundo.dart
@@ -6,6 +6,20 @@
  * @version 1.0.0
  */
 
+// INFO: sfsdfasdf asdf
+// DUDAR: sdfasdf asdf
+// HACER: asdfasdfas
+// MEJORAR: asdfa sfdasf
+// REVISAR: asdfasf asfd
+// ERROR: asdf asf d
+
+//- INFO asdfasdfasdf
+//? DUDAR asdfasdf
+//> HACER asdf asf
+//! REVISAR
+//* ERROR
+//// SDFKJSALKDF
+
 void main() {
   print('Hola Mundo');
 }
